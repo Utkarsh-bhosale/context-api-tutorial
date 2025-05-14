6a96359b08974faedeaef2bac0b3371cd0a86ef7
Utkarsh-bhosale <bhosaleutkarsh13@gmail.com>
2025-05-14 12:02:24 +0530

    Implemented context provider

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import UserContextProvider from "./context/UserContextProvider"
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      
+    </UserContextProvider>
+  )
+}
+
+export default App
