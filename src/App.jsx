167ca62978d070c227d2abdae10636755ac9937c
Utkarsh-bhosale <bhosaleutkarsh13@gmail.com>
2025-05-14 12:48:59 +0530

    Created login and Profile components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import Login from "./components/Login"
+import Profile from "./components/Profile"
 import UserContextProvider from "./context/UserContextProvider"
+import React from "react"
+import './App.css'
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = React.useState(0)
 
   return (
     <UserContextProvider>
-      
+      <Login/>
+      <Profile/>
     </UserContextProvider>
   )
 }
